e2d9f49d74856b5fc0351356c175094c3e4415da
Christopher Zaworski <christopherzaworski@gmail.com>
2019-01-21 11:09:38 -0500

    working on detect music alg, need to properly set up the file with times etc. to know whats really going on

--- matlab_code/detect_signalspec.m
+++ matlab_code/detect_signalspec.m
@@ -35,13 +35,37 @@ signal_indices_Test = []; %%This array is meant to test the detection algorithm
 for k = 1:size(s,2);
     low_freq = sum(s(1:5,k));
     hi_freq = sum(s(6:end,k));
-    if abs(hi_freq) > 0.5;
-        %signal_index = k;
+    if abs(hi_freq)/abs(low_freq) > 1;
+        %If there is sufficient high frequency content in the signal then keep track of
+        %these indexes in a buffer
         signal_indices_Test = [signal_indices_Test, k*winSize]; %%%THIS ARRAY TO TEST DETECTION
         signal_buffer = [signal_buffer, k*winSize];
+        continue;
     end
+    if length(signal_buffer) == 1; 
+        %If a signal is detected in one window, but not the next it is most likely a click
+        %add it to the clicks array and clear the current buffer
+        disp('CLICK BUFFER');
+        clicks = [clicks, signal_buffer];
+        signal_buffer = [];
+        continue;
+    end
+    if length(signal_buffer) > 1; 
+        %If a signal was detected and it stretches over more than a few windows, then add it 
+        %to the signal array as start and ending indices and clear the buffer
+        start_signal = signal_buffer(1); 
+        end_signal   = signal_buffer(length(signal_buffer)); 
+        signal = [signal;[start_signal, end_signal]];
+        signal_buffer = []; %clear the signal buffer array
+        continue; 
+    end
+    continue;
 end
 
+length(clicks)
+length(signal)
+clicks
+signal
 % [s,f,t] = spectrogram(data,fs)
 % imagesc (t, f, log(s));
 figure(1);
@@ -54,5 +78,22 @@ hold on;
 signal_indices_Test = transpose(signal_indices_Test);
 for xi = 1:size(signal_indices_Test)
     x1 = (signal_indices_Test(xi));
-    line([x1 x1], get(gca, 'ylim'),'Color', 'black','LineStyle', '--');
+    line([x1 x1], get(gca, 'ylim'),'Color', [0,0,0] + 0.8 ,'LineStyle', '--');
 end
+
+
+for i = 1:size(clicks);
+    x1 = (clicks(i));
+    line([x1 x1], get(gca, 'ylim'),'Color', 'blue','LineStyle', '-');
+end  
+
+for i = 1:size(signal);
+    x1 = (signal(i, 1));
+    x2 = (signal(i, 2));
+    line([x1 x1], get(gca, 'ylim'),'Color', 'red','LineStyle', '-'); 
+    line([x2 x2], get(gca, 'ylim'),'Color', 'green','LineStyle', '-');
+end  
+
+
+
+
