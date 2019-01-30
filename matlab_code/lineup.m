6922d502e85cf76accc036bd9a5b07b2ca79b4e0
Christopher Zaworski <christopherzaworski@gmail.com>
2019-01-30 13:43:20 -0500

    The lining up should work, need to loop through the files and test now

--- matlab_code/lineup.m
+++ matlab_code/lineup.m
@@ -1,5 +1,4 @@
-%reference = 'correlation1.wav'
-%filename = 'correlation2.wav'
+%reference = 'correlation1.wav' %filename = 'correlation2.wav'
 %
 %
 %[data_file, fs] = audioread(filename);
@@ -55,22 +54,25 @@ to work with multiple recordings of the same audio file and our groove plotting 
 
 path_ref = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/1015_18_LiteToneTest/5.1.wav';
 dir_files = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/1015_18_LiteToneTest/';
-name_files = {'5.2.wav', '5.3.wav', '5.4.wav', '5.5.wav'}; 
+name_files = {['5.2.wav', '5.3.wav', '5.4.wav', '5.5.wav']}; 
 
-AUDIO_FILES = [];
+AUDIO_FILES = {};
 
 [data_ref, fs_ref] = audioread(path_ref);
 time_ref = (0:length(data_ref)-1)/fs;
-
-AUDIO_FILES(:,:,1) = data_ref;
-
+data_ref = data_ref(:,1);
+size(data_ref)
+AUDIO_FILES{1} = data_ref;
+size(AUDIO_FILES)
+size(name_files)
 %~ Loop through all the files and line them up with the reference
 for i = (1:length(name_files)); 
+    strcat(dir_files,filename{i})
     [data_file, fs_file] = audioread(strcat(dir_files,filename{i}));
     time_file = (0:length(data_file)-1)/fs_ref; %Not sure if important, but here I'm using the 
                                              %fs of the reference file, I assume thats the right one
-    size(data_file)
-    size(data_ref)
+    
+    data_file = data_file(:,1);
     
     figure(1)
     subplot(2,1,1)
@@ -82,24 +84,26 @@ for i = (1:length(name_files));
     title('s_2')
     xlabel('Time (s)')
 
-   
-    data_file = data_file.';
-    data_ref = data_ref.'; 
-    [acor,lag] = xcorr(data_ref,data_file);
+    
+    [acor,lag] = xcorr(data_file,data_ref);
     [~,I] = max(abs(acor));
-    lagDiff = lag(I);
-    timeDiff = lagDiff/fs_ref;
+    lagDiff = lag(I)
+    timeDiff = lagDiff/fs_ref
     cdata_file = data_file(-lagDiff+1:end);
     ct_file = (0:length(cdata_file)-1)/fs_ref;
-    AUDIO_FILES(:,:,i+1) = cdata_file; 
+   
+    size(data_file)
+    size(data_ref)
+   
+    AUDIO_FILES{i+1} = cdata_file; 
 end
 
 %~~~Segmenting Parameters~~~%
 time = time_ref;
 rotation_speed = 33.33333;%45;
 T = 60/rotation_speed; %this is the length of one groove segment
-num_segs = (floor(length(data)/fs/T))
-n_sam = round(T*fs)
+num_segs = (floor(length(data_ref)/fs_ref/T))
+n_sam = round(T*fs_ref)
 time_seg = time(1:n_sam);
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
