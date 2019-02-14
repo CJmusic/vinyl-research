ee077c254af27f6d7576a586be15896574f003f8
Christopher Zaworski <christopherzaworski@gmail.com>
2019-02-14 09:49:24 -0500

    formatting mostly

---
+++ matlab_code/audio_analysis.m
@@ -0,0 +1,105 @@
+%{ This script contains all the functions needed for anaylsis 
+%
+%
+%%} 
+
+
+%~~~~~~~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~%
+
+%{ LOAD FILES %}
+
+[data_ref, fs_ref] = audioread(path_ref);
+data_ref = data_ref(:,1);
+%data_ref = data_ref(1:5*fs_ref);
+size(data_ref)
+time_ref = (0:length(data_ref)-1)/fs_ref;
+AUDIO_FILES{1} = data_ref;
+size(AUDIO_FILES)
+size(name_files)
+name_files
+figure(1); hold on;
+plot(time_ref,data_ref,'g')
+%~ Loop through all the files and line them up with the reference
+
+
+%{ LINEUP %}
+for i = (1:length(name_files));
+ 
+    strcat(dir_files,name_files{i})
+    [data_file, fs_file] = audioread(strcat(dir_files,name_files{i}));
+    
+    data_file = data_file(:,1);
+     
+    time_file = (0:length(data_file)-1)/fs_file; %Not sure if important, but here I'm using the fs
+    
+    [acor,lag] = xcorr(data_file,data_ref);
+    [~,I] = max(abs(acor));
+    lagDiff = lag(I)
+    timeDiff = lagDiff/fs_file
+    cdata_file = data_file(lagDiff+1:end);
+    ctime_file = (0:length(cdata_file)-1)/fs_file;
+
+    % plot(ctime_file, cdata_file)
+    % title('Original Audio Lined up')
+    % xlabel('Time (s)')
+    % AUDIO_FILES{i+1} = cdata_file; 
+end
+
+
+%{ SPECTRUM PLOT %}
+
+
+%{ GROOVE PLOT %}
+
+
+clf(figure(1))
+clf(figure(2))
+AUDIO_FILES = {};
+
+%~~~Segmenting Parameters~~~%
+time = time_ref;
+rotation_speed = 33.33333;%45;
+T = 60/rotation_speed; %this is the length of one groove segment
+n_sam = round(T*fs_ref)
+time_seg = time(1:n_sam);
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+
+%~~Detect_Signal Parameters~~~%
+winSize= 2^8;%round(fs*dur);
+overlap=0;%round(winSize/2);
+fftsize=2^8;%winSize;
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+
+%~~~~~GROOVE PLOTTING~~~~~~%
+seg_array = [];
+name_files= [ {'5.2.wav'},name_files];
+
+figure(2);hold on; legend;
+for i=(1:length(AUDIO_FILES))
+    data = AUDIO_FILES{i};
+    num_segs = (floor(length(data)/fs_ref/T))
+    for ng = 1:num_segs
+        %seg_array(:,:,ng) = data(1+(ng-1)*n_sam:ng*n_sam,:);
+        data_seg = data(1+(ng-1)*n_sam:ng*n_sam,:);
+        [s,f,t] = spectrogram(data_seg,winSize,overlap,fftsize,fs_file,'yaxis');
+        lo_freq = sum(s(1:5));
+        hi_freq = sum(s(6:end));
+        %if abs(hi_freq/lo_freq) < 2.0; %%This is where the DETECT SIGNAL algorithm will go once its active
+        if ng > 1 &  ng < 4; %for now just plot grooves 2 & 3  
+            plot(time_seg,data_seg,  'DisplayName', [ name_files{i},'groove', num2str(ng)]);
+        end
+    end    
+end
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+%~~~~~~~~~~~~~~~~~~FUNCTIONS END~~~~~~~~~~~~~~~~~~~~~~%
+
+
+
+%{ SCRIPT START BELOW %}
+%
+path_ref = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/1015_18_LiteToneTest/5.2.wav';
+dir_files = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/1015_18_LiteToneTest/';
+name_files = {'5.1.wav'};  % {['5.2.wav', '5.3.wav', '5.4.wav', '5.5.wav']}; 
---
+++ matlab_code/digital_lineup.m
@@ -0,0 +1,78 @@
+
+
+path_digital_files = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/00_digital_files/'; 
+
+sweep_file = 'sweep16kHz.wav';
+tone100_file = 'tone100.wav';
+tone1000_file = 'tone1000.wav'; 
+tone3150_file = 'tone3150.wav';
+tone10000_file = 'tone10000.wav';
+
+%%{
+%
+%TRACK LISTINGS ON TEST RECORDS 
+%00:02 : 1KHz@7cm-s lateral
+%01:02 : 10kHz @ -20dB
+%01:32 : 100Hz
+%02:04 : frequency sweep 20Hz-16kHz 
+%02:40 : quiet groove
+%03:02 : 3150 wow & flutter
+%04:08 : 1kHz left
+%04:28 : frequency sweep left
+%05:06 : 1kHz right
+%05:26 : frequency sweep right
+%06:04 : 1kHz vertical
+%06:24 : frequency sweep vertical
+%
+%%}
+
+%name_files = {[tone1000_file, tone10000_file, tone100_file, sweep_file %{, quiet groove %}, tone3150_file, tone1000_file %{left%}, sweep_file %{left%}, tone1000_file %{right%}, sweep_file %{right%}, tone1000_file %{vertical%}, sweep_file %{vertical%}]};
+
+name_files = {tone1000_file, tone10000_file, tone100_file, sweep_file  tone3150_file, tone1000_file  sweep_file, tone1000_file, sweep_file, tone1000_file, sweep_file};
+
+lagDiff_lists = [0];
+
+
+
+
+%now reference is recording
+
+recorded_file = '/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_files/A0000B0000/02072019_A0000B000r25-A.wav'; %path to the recording of the record
+
+[data_rec, fs_rec] = audioread(recorded_file); 
+time_rec = (0:length(data_rec)-1)/fs_rec;
+
+data_rec = data_rec(:,1); %take only the first channel
+clf(figure(1))
+figure(1) 
+hold on; grid on; 
+plot(time_rec,data_rec, 'g')
+
+
+for i = (1:length(name_files));
+
+    start_offset = lagDiff_lists(end);%[length(lagDiff_lists)];
+    % look at the last entry in the lagDiff, maybe replace this with the ending sample
+     
+    strcat(path_digital_files,name_files{i})
+    [data_file, fs_file] = audioread(strcat(path_digital_files,name_files{i}));
+    
+    data_file = data_file(:,1);
+     
+    time_file = (0:length(data_file)-1)/fs_file; %Not sure if important, but here I'm using the fs
+    
+    [acor,lag] = xcorr(data_file,data_rec);
+    [~,I] = max(abs(acor));
+    lagDiff = lag(I)
+    timeDiff = lagDiff/fs_file
+    cdata_file = data_file(-lagDiff+1:end);
+    ctime_file = (lagDiff:lagdiff + length(cdata_file)-1)/fs_file;
+    lagDiff_lists = [lagDiff_lists, lagDiff]; %append to the list of all lag diffs to keep track of when each signal starts
+    plot(ctime_file, cdata_file)
+    title('Original Audio Lined up')
+    xlabel('Time (s)')
+    AUDIO_FILES{i+1} = cdata_file; 
+end
+
+
+
