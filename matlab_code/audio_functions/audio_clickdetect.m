7e3a8243fa67e10f4cd1b5bdbc7a57a1378078ea
Christopher Zaworski <christopherzaworski@gmail.com>
2019-03-07 13:03:40 -0500

    cleaned up the click detect function

--- matlab_code/audio_functions/audio_clickdetect.m
+++ matlab_code/audio_functions/audio_clickdetect.m
@@ -10,6 +10,7 @@ clicks = [ sample numbers ]
 function clicks = audio_clickdetect(data, fs);
    time = (0:length(data)-1)/fs; 
    d_data = diff(data);%/diff(time);
+   dd_data = diff(d_data);%/diff(time);
    clicks = [];
    for i = (1:length(data));
       if i + 1024 < length(data);
@@ -24,17 +25,17 @@ function clicks = audio_clickdetect(data, fs);
    
    %plotting below
    
-   x = zeros(length(clicks));
-   figure(1);
-   plot(time, data); grid on; hold on;
-   plot(clicks/fs,x, 'r.', 'MarkerSize', 20);
-   figure(2); 
-   subplot(2,1,1);
-   plot(time(1:end-1),d_data); grid on; hold on;
-   plot(clicks/fs,x,'r.', 'MarkerSize', 20);
-   subplot(2,1,2); 
-   plot(time(1:end-2),dd_data); grid on; hold on;
-   plot(clicks/fs,x,'r.', 'MarkerSize', 20);
+  % x = zeros(length(clicks));
+  % figure(1);
+  % plot(time, data); grid on; hold on;
+  % plot(clicks/fs,x, 'r.', 'MarkerSize', 20);
+  % figure(2); 
+  % subplot(2,1,1);
+  % plot(time(1:end-1),d_data); grid on; hold on;
+  % plot(clicks/fs,x,'r.', 'MarkerSize', 20);
+  % subplot(2,1,2); 
+  % plot(time(1:end-2),dd_data); grid on; hold on;
+  % plot(clicks/fs,x,'r.', 'MarkerSize', 20);
 end 
 
 
