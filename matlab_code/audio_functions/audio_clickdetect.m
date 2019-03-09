52317a7f0709f84bc8799eea5c4f43e4dd8620c7
Christopher Zaworski <christopherzaworski@gmail.com>
2019-03-09 17:12:05 -0500

    click detect looking good and plotting working properly

--- matlab_code/audio_functions/audio_clickdetect.m
+++ matlab_code/audio_functions/audio_clickdetect.m
@@ -9,8 +9,7 @@ clicks = [ sample numbers ]
 
 function clicks = audio_clickdetect(data, fs);
    time = (0:length(data)-1)/fs; 
-   d_data = diff(data);%/diff(time);
-   dd_data = diff(d_data);%/diff(time);
+   d_data = diff(data,1)*fs;% since delta_t = 1/fs:w;
    clicks = [];
    for i = (1:length(data));
       if i + 1024 < length(data);
@@ -22,33 +21,33 @@ function clicks = audio_clickdetect(data, fs);
           end
       end
    end
-   clf(figure(1));clf(figure(2)); 
    disp('Number of clicks');
    size(clicks) 
+   
    %plotting below
    
-  % x = zeros(length(clicks));
-   figure(1);
-   plot(time, data); grid on; hold on;
-   
+   clf(figure(1));clf(figure(2)); 
+   figure(1); grid on; hold on;
+   plot(time, data); 
     for xi = 1:length(clicks);
-        x1 = time(clicks(xi));
-        line([x1 x1], get(gca, 'ylim'),'Color', 'black','LineStyle', '--');
+         x1 = time(clicks(xi));
+         line([x1 x1], get(gca, 'ylim'),'Color', 'black','LineStyle', '--');
     end
-   figure(2); 
-   subplot(2,1,1);
-   plot(time(1:end-1),d_data); grid on; hold on;
+    title('Click detection: Amplitude vs. Time'); 
+    xlabel('Time [s]');
+    ylabel('Amplitude');
 
-    for xi = 1:length(clicks);
+   figure(2); grid on; hold on;
+   t_diff = length(time) - length(d_data)
+   plot(time(1:end-t_diff),d_data); grid on; hold on;
+   for xi = 1:length(clicks);
         x1 = time(clicks(xi));
         line([x1 x1], get(gca, 'ylim'),'Color', 'black','LineStyle', '--');
     end
-   subplot(2,1,2); 
-   plot(time(1:end-2),dd_data); grid on; hold on;
-    for xi = 1:length(clicks);
-        x1 = time(clicks(xi));
-        line([x1 x1], get(gca, 'ylim'),'Color', 'black','LineStyle', '--');
-    end
+
+    title('Click detection: 1st Derivative vs. Time'); 
+    xlabel('Time [s]');
+    ylabel('dA/dt (s^-1)');
 end 
 
 
