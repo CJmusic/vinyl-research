9964ee6a69115771d2dfe2e5b48911a22889b9f7
Christopher Zaworski <christopherzaworski@gmail.com>
2019-03-13 00:16:35 -0400

    added click matrix file

---
+++ matlab_code/audio_functions/audio_clickmatrix.m
@@ -0,0 +1,14 @@
+
+
+% this function looks through the clicks in two audio files and generates the click matrix 
+% last edit : march 12 2018
+
+function diff_array = audio_clicklineup(clicks_ref, clicks);
+    diff_array = []; % this array contains the distances between every click, each row 
+                     % represents a click in the referenc each column represents a click in the file being looked at 
+    for xi = (1:length(clicks_ref));%this makes an array with the distance between each click in the two
+                                    % files
+        diff_array = [diff_array; clicks - clicks_ref(xi)];
+    end
+    lagdiff = mode(diff_array(:)); % the time difference between the two signals is the most common distance between clicks
+end
