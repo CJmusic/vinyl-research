c67472c21fcdce57496dfc56d5c13f77119372bd
Christopher Zaworski <christopherzaworski@gmail.com>
2019-03-16 21:02:57 -0400

    dictionary made succefully, need to make sure if I've it properly. then implement the reference_recordclass subclass

--- matlab_code/record_process.m
+++ matlab_code/record_process.m
@@ -44,7 +44,7 @@ signal_array = audio_detectsignal(reference.dataL);
 
 %%% RECORD INFO 
 %signal_names = {'leadin','1kHz', '10kHz', '100Hz', 'freqsweep', 'quiet', '3150Hz', '1kHzL', 'swepL', '1kHzR', 'sweepR', '1kHzV', 'sweepV', 'extra_signal','leadout'}; 
-signal_names = {'leadin','1kHz', '10kHz', '100Hz', 'freqsweep', 'quiet', '3150Hz', '1kHzL', 'swepL', '1kHzR', 'sweepR', '1kHzV', 'sweepV','transition','leadin2','1kHz2', '10kHz2', '100Hz2', 'freqsweep2', 'quiet2', '3150Hz2', '1kHzL2', 'swepL2', '1kHzR2', 'sweepR2', '1kHzV2', 'sweepV2','leadout2'};
+signal_names = {'needledrop','leadin','1kHz', '10kHz', '100Hz', 'freqsweep', 'quiet', '3150Hz', '1kHzL', 'swepL', '1kHzR', 'sweepR', '1kHzV', 'sweepV','transition','leadin2','1kHz2', '10kHz2', '100Hz2', 'freqsweep2', 'quiet2', '3150Hz2', '1kHzL2', 'swepL2', '1kHzR2', 'sweepR2', '1kHzV2', 'sweepV2','leadout2'};
 ref_timestamps = [0, 2, 62, 92, 124, 160, 182, 248, 268, 306, 326, 364, 384, 419.5];% this is how many seconds each signal is according to Chris Muth's track listing
 lengths = [60, 30, 31, 36, 21, 66, 20, 37, 19, 37, 19, 37, 19]; %starts with 1kHz
 
@@ -67,12 +67,8 @@ ref_signalsL = [];
 ref_signalsR = [];
 ref_signals = {};
 
-%   newMap = containers.Map(keys, values);
-
-
 ref_signalsL = reference.dataL(1:floor((ref_timestamps(1))*reference.fs));
 ref_signalsR = reference.dataR(1:floor((ref_timestamps(1))*reference.fs));
-
 ref_signals{end + 1} = [ref_signalsL, ref_signalsR];
 
 % first set of signals on the disk
@@ -99,13 +95,13 @@ for i = (1:length(ref_timestamps2)-1);
     ref_signals{end + 1} = [ref_signalsL, ref_signalsR];
 end
 
-
+% get the rest of the file
 ref_signalsL = reference.dataL(floor(ref_timestamps2(end)*reference.fs):end);
-ref_signalsR = reference.dataR(floor(ref_timestamps(end)*reference.fs):end);
+ref_signalsR = reference.dataR(floor(ref_timestamps2(end)*reference.fs):end);
+ref_signals{end + 1} = [ref_signalsL, ref_signalsR];
 
 size(ref_signals)
 size(signal_names)
-ref_signals
 
 % The loop below is for info only 
 for i = (1:length(ref_signals)); 
@@ -119,6 +115,21 @@ for i = (1:length(ref_signals));
 %    length(ref_signals(i,:,2))/reference.fs
 end
 
+
+%   newMap = containers.Map(keys, values);
+ref_tracks = containers.Map(signal_names, ref_signals);
+
+
+names = keys(ref_tracks);
+datas = values(ref_tracks);
+
+for i = (1:length(ref_tracks));
+    %ref_tracks(i)
+    disp('next track')
+    names{i} 
+    length(datas{i})/reference.fs
+
+end
 
 % looks like ref_signals is 1x26 
 % and signals is 1x15
