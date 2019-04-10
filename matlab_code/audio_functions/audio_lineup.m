30436cdf6ca1c6aa63fdd052b167cc2b833c2d30
Christopher Zaworski <christopherzaworski@gmail.com>
2019-04-09 23:18:22 -0400

    fixed a bug where the lagdiff was the index not the value

--- matlab_code/audio_functions/audio_lineup.m
+++ matlab_code/audio_functions/audio_lineup.m
@@ -13,39 +13,19 @@
 %
 %function [cdata, ctime] = audio_lineup(data_file, fs_file, time_file, data_ref)
 function lagDiff = audio_lineup(data_file, data_ref, fs_file)
-    %size(data_file)
-    %size(data_ref)
-   % data_ref_L = data_ref(:,1); 
-   % data_ref_R = data_ref(:,2); 
-   % data_file_L = data_file(:,1); 
-   % data_file_R = data_file(:,2); 
-   
     disp('audio_lineup function')
-    size(data_file)
-    size(data_ref)
 
    % [acor,lag] = xcorr(data_file_L,data_ref_L);
-    [acor_L,lag_L] = xcorr(data_file(:,1),data_ref(:,1));
-    [acor_R,lag_R] = xcorr(data_file(:,2),data_ref(:,2));
+    [acor_L,lags_L] = xcorr(data_file(:,1),data_ref(:,1));
+    [acor_R,lags_R] = xcorr(data_file(:,2),data_ref(:,2));
     figure(50); hold on; grid on; 
     plot(acor_L)
     plot(acor_R,'--')
 
-
-    %disp('lag_L')
-    %max(lag_L) 
-    %disp('lag_R')
-    %max(lag_R)
-
-    %figure(3); hold on; grid on;
-    %plot(acor)
     [~,I_L] = max(abs(acor_L));
-    %lagDiff_L = lag(I_L);
-    lagDiff_L = I_L
+    lagDiff_L = lags_L(I_L);
     [~,I_R] = max(abs(acor_R));
-    lagDiff_R = I_R
-    % lagDiff_R = lag(I_R);
-    % timeDiff = lagDiff/fs_file;
+    lagDiff_R = lags_R(I_R);
     
     disp('lagDiff_L')
     lagDiff_L
