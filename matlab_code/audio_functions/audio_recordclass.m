92c0680fd3398f6e2cfca934d01f9af508d840a0
Christopher Zaworski <christopherzaworski@gmail.com>
2019-04-12 10:38:16 -0400

    got proper offset for r27, weirdly I still have the needle drop in the tracks-- that shouldn't be there so I'm not counting hte proper num of tracks

--- matlab_code/audio_functions/audio_recordclass.m
+++ matlab_code/audio_functions/audio_recordclass.m
@@ -28,12 +28,14 @@ classdef audio_recordclass < handle %inheriting handle allows methods to update 
         signal_times = {};
         
         %%% RECORD INFO 
-        timestamps = [0, 2, 62, 92, 124, 160, 182, 248, 268, 306, 326, 364, 384, 419.5];
+        % timestamps = [0, 2, 62, 92, 124, 160, 182, 248, 268, 306, 326, 364, 384, 419.5];
+        timestamps = [0, 60, 90, 122, 158, 180, 246, 266, 304, 324, 362, 382, 417.5];
         % this is how many seconds each signal is according to Chris Muth's track listing
         lengths = [60, 30, 31, 36, 21, 66, 20, 37, 19, 37, 19, 37, 19]; %starts with 1kHz
         signal_names = {'needledrop','leadin','1kHz', '10kHz', '100Hz', 'freqsweep', 'quiet', '3150Hz', '1kHzL', 'sweepL', '1kHzR', 'sweepR', '1kHzV', 'sweepV','transition','leadin2','1kHz2', '10kHz2', '100Hz2', 'freqsweep2', 'quiet2', '3150Hz2', '1kHzL2', 'sweepL2', '1kHzR2', 'sweepR2', '1kHzV2', 'sweepV2','leadout2'};
 
-        offset = 4.25; % as measured on /020818_A0000B0000/02072019_A0000B000r25-A.wav
+        % offset = 4.25; % as measured on /020818_A0000B0000/02072019_A0000B000r25-A.wav
+        offset = 10.625; % as measured on /020818_A0000B0000/02072019_A0000B000r27-A.wav
         %time = (0:length(dataL)-1)/rec.fs;
         transition = 518.25; % as measured on /020818_A0000B0000/02072019_A0000B000r25-A.wav
         lagdiff = 0;
