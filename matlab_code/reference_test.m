698b7b711c7001aea8d15a9cc8c1f4211a0576b7
Christopher Zaworski <christopherzaworski@gmail.com>
2019-04-12 09:34:06 -0400

    made reference test, plots all the track in squential order with proper time

---
+++ matlab_code/reference_test.m
@@ -0,0 +1,42 @@
+% This file is meant to test the slicing of the reference file as well as the lining up of records to this reference and their slicing afterwards
+%
+% christopher zaworski
+% last edit : april 11, 2019
+%
+%
+
+
+disp('----------reference_test.m-------------')
+clc;close all;
+addpath('audio_functions')
+addpath('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/');
+
+% path_folder = strcat('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/', folder)
+
+references = {'/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000/031418_A0000B0000r27a.wav'};    
+
+
+reference_file = references{1}
+reference = audio_refrecordclass(reference_file)
+
+
+time_offset = 0;
+track_names = keys(reference.tracks) ;
+track_data  = values(reference.tracks) ;
+
+figure(1); hold on; grid on;
+for i = (1:length(reference.tracks));
+    track = track_data{i}.';%'
+    time = (time_offset:time_offset + length(track)-1)/reference.fs;%
+    % if i > 1;
+    %     time_offset = time_offset + time(end);
+    % end
+    time(1)
+    time(end)
+    size(time)
+    time_offset = time_offset + length(time);
+    % time = time + time_offset;
+    time_offset
+    plot(time, track);
+    time = [];
+end
