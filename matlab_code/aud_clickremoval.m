b21520f11e254a3e1d126b19275c58b767637e23
Christopher Zaworski <christopherzaworski@gmail.com>
2019-05-17 15:59:31 -0400

    still trying to understand audacity's click detect/removal. Kinda close, and its been translated to matlab however I still don't understand it

--- matlab_code/aud_clickremoval.m
+++ matlab_code/aud_clickremoval.m
@@ -1,6 +1,6 @@
 % bool EffectClickRemoval::ProcessOne(int count, WaveTrack * track, sampleCount start, sampleCount len)
-% function aud_clickremoval(buffer)
-function buffer = aud_clickremoval(buffer, fs)
+% function aud_clickremoval(data)
+function [data, clicks] = aud_clickremoval(data, fs)
     %%% Variables from other files
 
     % Param( Threshold, int,     wxT("Threshold"),  200,     0,       900,     1  );
@@ -13,9 +13,9 @@ function buffer = aud_clickremoval(buffer, fs)
 
     %%%%
 
-    % in the matlab code buffer is the array of buffer
-    % buffer = buffer; 
-    len = length(buffer); 
+    % in the matlab code data is the array of data
+    % data = data; 
+    len = length(data); 
     % I believe the above are the two inputs in the audacity functions;
 
 
@@ -25,78 +25,123 @@ function buffer = aud_clickremoval(buffer, fs)
     % j = 0;
     left = 0;
 
+    s2 = int16(sep/2); % originally assigned up above but might not need to do it, this is simply 
     % msw = 0.0;
     ww = 0;
+    num_clicks = 0;
+    % clicks = [];
 
-    ms_seq = zeros(length(buffer),2);
-    b2 = zeros(len,2);
+    data_energy = zeros(len,2);  %%data_energy is the signals energy
 
-    % b2 is the power of the signal
+    % data_energy is the power of the signal
     for i=(1:len) % calculate the rms level
-        b2(i) = buffer(i)*buffer(i);
+        data_energy(i) = data(i)*data(i);
     end
 
-    for i=(1:i*i:sep) %% in c++ 
+    % ms_seq = zeros(length(data),2);
+    ms_seq = data_energy;
+    % for i = (1:len)
+        % ms_seq(i]=b2[i];
+
+    % for i=(1:i*i:sep) %% in c++ 
+    i = 1
+    while i <= sep
+        i = i*2;
         for j=(1:len-i)
             ms_seq(j) = ms_seq(j) + ms_seq(j+i);
         end
     end
+    disp('after ms_seq set')
+    % ms_seq
 
     disp('GOING INTO 4 LOOP')
     % sep looks to be 2^8, probably the width of the samples being looked at
+    disp('length data')
+    size(data)
+    disp('len-sep')
+    len-sep
+
+    clicks = [];
+
+
     for i=(1:len-sep)
         % size(ms_seq(i))
         % size(sep)
         % size(ms_seq)
-        ms_seq(i) = sep/ms_seq(i);
+        ms_seq(i) = ms_seq(i)/sep;
     end
 
     % truncate sep to the next lowest power of two
     % sep appears to be the len
     % sep = i; 
-    s2 = int16(sep/2); % originally assigned up above but might not need to do it, this is simply 
-                % length of buffer/2  
+                % length of data/2  
     % wrc = 0;
 
     %%% this is a for loop in the C++ but probably needs to be converted to a while loop 
 %    for(wrc=mClickWidth/4; wrc>=1; wrc /= 2) {
     wrc = mClickWidth/4;
+    disp('Final While loop')
     while wrc >= 1
+        %% wrc looks like a resolution, so it loops through the file multiple times at each 
+        %  ClickWidth (ie: resolution) and tests it to see if there is a click there
         wrc = wrc/2;
-        ww = mClickWidth/wrc; % 
-    end %while wrc loop
+        
+        %% ww is the variable ClickWidth without units
+        ww = mClickWidth/wrc;  
 
-    for i=(1:len-sep);
-        msw = 0;
-        for j=(1:ww+1)
-            msw = msw + b2(i+s2+j); %% msw seems to be a sum of the power at weird indeces
-                                    %  or using some weird c++ syntax 
-        end
-        msw = msw/ww;
-
-
-        %% this is the point where clicks are detected and replaced in 
-        %  the buffer = the list of buffer 
-        %  ms_seq = main test for clicks? 
-        if msw >= mThresholdLevel * ms_seq(i)/10;
-            if left == 0;
-                left = i+s2; %% this is where left gets assigned its initial value
+        for i=(1:len-sep);
+            %% len-sep simply avoids the last iteration I believe
+            
+            % i
+            msw = 0;
+            for j=(1:ww+1)
+                msw = msw + data_energy(i+s2+j); %% msw seems to be a sum of the power at weird indices
+                                        %  or using some weird c++ syntax 
             end
-        else;
-            if(left ~= 0 && (i-left) <= ww*2); %% ww ClickWidth/4
-                lv = buffer(left);
-                rv = buffer(i+ww+s2);
-                for j = (left:i+ww+s2)
-                    bResult = true;
-                    %% The line below replaces the sample in the buffer with I think
-                    %  is the average of the remaining samples being analyzed
-                    buffer(j) = (rv*(j-left) + lv*(i+ww+s2-j))/(i+ww+s2-left);
-                    b2(j) = buffer(j)*buffer(j)
+            msw = msw/ww;
+
+
+            %% this is the point where clicks are detected and replaced in 
+            %  the data = the list of data 
+            %  ms_seq = main test for clicks? 
+            % disp('msw')
+            % msw
+            % disp('mThresholdLevel * ms_seq(i)/10 ')
+            % mThresholdLevel * ms_seq(i)/10 
+            if msw >= mThresholdLevel * ms_seq(i)/10
+                % data(i) = 0;
+                % num_clicks = num_clicks + 1;
+                % clicks(num_clicks) = i;   
+                if left == 0
+                    % disp('LEFT 0')
+                    % disp('CLICK DETECTED?')
+                left = i+s2; %% this is where left gets assigned its initial value
                 end
-            elseif(left ~= 0)
+                elseif (left ~= 0 && (i-left) <= ww*2); %% ww ClickWidth/4
+                % disp('i-left')
+                % i-left
+                % disp('ww*2')
+                % ww*2
+                if ((i-left) <= ww*2); %% ww ClickWidth/4
+                    % disp('CLICK BEING REMOVED')
+                    lv = data(left);
+                    rv = data(i+ww+s2);
+                    for j = (left:i+ww+s2)
+                        bResult = true;
+                        %% The line below replaces the sample in the data with I think
+                        %  is the average of the remaining samples being analyzed
+                        data(j) = (rv*(j-left) + lv*(i+ww+s2-j))/(i+ww+s2-left);
+                        % data(j) = 1.0;
+                        data_energy(j) = data(j)*data(j);
+                    end
                 left = 0;
-            end % if left ~=
-        end %if msw >=
+                elseif(left ~= 0)
+                    left = 0;
+                % end % if left ~=
+            end %if msw >=
+
+            % data(i) = 0;
+        end %while wrc loop
     end % end for loop
     % return bResult
     % Comment from Audacity writer
@@ -119,44 +164,44 @@ function buffer = aud_clickremoval(buffer, fs)
 
 
 
-    % "below is the function ProcessOne translated, as far as I can tell it really just cleans up the wave buffer from Audacity and gets things ready to be processed. Not needed for our case" 
+    % "below is the function ProcessOne translated, as far as I can tell it really just cleans up the wave data from Audacity and gets things ready to be processed. Not needed for our case" 
 %     if(len <= windowSize/2);
 %         disp("windowSize must be bigger than ", windowSize/2);
 %     end
 
 %     idealBlockLen = track.GetMaxBlockSize * 4; 
 
-%     " as far as I can tell this is simply a maximum number of samples in the buffer "
+%     " as far as I can tell this is simply a maximum number of samples in the data "
 
 % %    auto idealBlockLen = track->GetMaxBlockSize() * 4;
 % %    if (idealBlockLen % windowSize ~= 0)
 % %       idealBlockLen += (windowSize - (idealBlockLen % windowSize));
 
-%     "the lines above confirm and simply set the block length to a factor of the max buffer size"
+%     "the lines above confirm and simply set the block length to a factor of the max data size"
 
 % %    bool bResult = true;
 % %    decltype(len) s = 0;
-% %    Floats buffer{ idealBlockLen };
-% %    Floats bufferwindow{ windowSize };
+% %    Floats data{ idealBlockLen };
+% %    Floats datawindow{ windowSize };
 
 %     "main iteration loop below: "
 
-%     while (length(buffer) - s) > windowSize/2;
+%     while (length(data) - s) > windowSize/2;
 %         %% in the audacity code this is a call to a function, I've replaced it with the output of the actual function
-%         block = [min( length( bufferSize ), 
-%         max( sampleCount(0), length(buffer - s))];
+%         block = [min( length( dataSize ), 
+%         max( sampleCount(0), length(data - s))];
         
 %         track()
-%         buffer = 0;
+%         data = 0;
 %         floatSample;
 %         start + s 
 %         block
 
-% %       track->Get((samplePtr) buffer.get(), floatSample, start + s, block);
+% %       track->Get((samplePtr) data.get(), floatSample, start + s, block);
 
 %         "unless this Get command is doing something special, this is simply returning the values as listed"
 
-%         %% I believe that the block is the main array of audio buffer
+%         %% I believe that the block is the main array of audio data
 %         for i = (1:windowSize/2:block);
 %         % for (1: i+windowSize/2 - 1 < block, i+= windowSize/2);
 
@@ -164,26 +209,26 @@ function buffer = aud_clickremoval(buffer, fs)
 % %          auto wcopy = std::min( windowSize, block - i );
 %             wcopy = min(windowSize, block - i);
 %             for (1:wcopy-1);
-%                 bufferwindow[j] = buffer[i+j];
+%                 datawindow[j] = data[i+j];
 %             end
 %             for (wcopy:windowSize);
-%                bufferwindow[j] = 0; 
+%                datawindow[j] = 0; 
 %             end
 
 %         %    mbDidSomething = or()      
 %         %    if mbDidSomething = false; 
-%             if RemoveClicks(windowSize, bufferwindow) ~= false;
+%             if RemoveClicks(windowSize, datawindow) ~= false;
 %                 mbDidSomething = true;
 %             end
 
 %             for j=(0:wcopy)
-%                 buffer[i+j] = bufferwindow;
+%                 data[i+j] = datawindow;
 %             end
 
 %             "this mbDidSomething variable is probably useless in our needs"
 %             % if mbDidSomething = True;
 %             %     track.set()
-%             %     samplePtr) buffer.get(), floatSample, start + s, block)
+%             %     samplePtr) data.get(), floatSample, start + s, block)
 %             % end
 
 %             s += block;
