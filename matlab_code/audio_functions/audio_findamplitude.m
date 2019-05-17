273080c69195e7087c3fae7e752ed8034dd9079a
Christopher Zaworski <christopherzaworski@gmail.com>
2019-05-17 16:06:26 -0400

    made a simple untested detect amplitude function

--- matlab_code/audio_functions/audio_findamplitude.m
+++ matlab_code/audio_functions/audio_findamplitude.m
@@ -7,12 +7,12 @@
 %
 %
 
-function amplitude = audio_findamplitude(data,freq,fs);
+function [amplitude] = audio_findamplitude(data,freq,fs)
     % frequency is the frequency of the sine wave 
     % you're looking for
-    peak = [];
-    for i = (1, length(data),freq*fs);
-       peak(i) = max(data); 
+    wav_peaks = [];
+    for i = (1, length(data)-1,freq*fs);
+       wav_peaks(i) = max(data(i:i+freq*fs)); 
     end % for loop
     amplitude = mean(peak);
 
