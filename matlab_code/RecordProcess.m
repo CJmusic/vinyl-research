37b92022326834e47bee2671711173d996f63597
Christopher Zaworski <christopherzaworski@gmail.com>
2019-09-11 13:35:31 -0400

    added new RecordProcess.m file

---
+++ matlab_code/RecordProcess.m
@@ -0,0 +1,16 @@
+
+
+addpath('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000')
+%% load audio 
+[data, fs] = audioread('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000/031418_A0000B0000r27a.wav') 
+figure(1)
+plot(data)
+%% load reference leadout 
+
+
+%% lineup audio with reference 
+
+
+%% get 1 kHz level and normalize 
+
+
