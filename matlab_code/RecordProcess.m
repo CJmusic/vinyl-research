1825591b4cb6eb18024e74403a9cfe7c2edc876d
Christopher Zaworski <christopherzaworski@gmail.com>
2019-09-12 09:30:54 -0400

    pushing changes to windows machine

--- matlab_code/RecordProcess.m
+++ matlab_code/RecordProcess.m
@@ -2,15 +2,51 @@
 
 addpath('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000')
 %% load audio 
-[data, fs] = audioread('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000/031418_A0000B0000r27a.wav') 
+[data, fs] = audioread('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000/031418_A0000B0000r27a.wav');
 figure(1)
 plot(data)
+
 %% load reference leadout 
+[ref, fs] = audioread('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/A0000B0000/031418_A0000B0000r27a.wav');
 
+timestamps_ref = [0, 60, 90, 122, 158, 180, 246, 266, 304, 324, 362, 382, 417.5];
+% this is how many seconds each signal is according to Chris Muth's track listing
+lengths = [60, 30, 31, 36, 21, 66, 20, 37, 19, 37, 19, 37, 19]; %starts with 1kHz
+signal_names = {'leadin','1kHz', '10kHz', '100Hz', 'sweep', 'quiet', '3150Hz', '1kHzL', 'sweepL', '1kHzR', 'sweepR', '1kHzV', 'sweepV','transition', '1kHz2', '10kHz2', '100Hz2', 'freqsweep2', 'quiet2', '3150Hz2', '1kHzL2', 'sweepL2', '1kHzR2', 'sweepR2', '1kHzV2', 'sweepV2','leadout'};
 
+%% Reference 02072019_A0000B000r27a.wav 
+offset = 10.625; 
+transition = 517.375; 
+% lockoutClipped = 953.746;
+lockout = 953.770; 
+refLockout = ref(floor(lockout*fs):end);
 %% lineup audio with reference 
+dataLockout = data(floor(950*fs):end);
+% lagdiff = []
+disp('sizes')
+size(data)
+size(ref)
 
+size(refLockout)
+size(dataLockout)
 
+figure(2); hold on; grid on;
+title('pre-lineup')
+plot(refLockout)
+plot(dataLockout)
+
+% [acor_L,lags_L] = xcorr(refLockout(:,1),dataLockout(:,1));
+% [M_L,I_L] = max(abs(acor_L));
+% lagdiff_L = lags_L(I_L);
+% lagdiff = lagdiff_L;
+% disp('lagdiff')
+% lagdiff 
+% datacorr = circshift(data,lagdiff);
+
+% figure(3); hold on; grid on;
+% title('post-lineup')
+% plot(refLockout)
+% plot(datacorr)
 %% get 1 kHz level and normalize 
 
 
