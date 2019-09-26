2aa231656aeb42988b8cdda226ff395aeefeb2f8
cz <christopherzaworski@gmail.com>
2019-09-26 12:23:53 -0400

    csv file now properly writes, need to clean up code and fix how clicks are reported

--- matlab_code/BatchProcess.m
+++ matlab_code/BatchProcess.m
@@ -8,10 +8,11 @@ folder = '';
     % files = dir('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/click_testing/*.wav')
 % end
 
-headers = {'date', 'pressing', 'top_stamper', 'top_hits', 'bottom_stamper', 'bottom_hits', 'record', 'side', 'track', 'lagdiff', 'normalization', 'RMS_L', 'RMS_R', 'clicks_L', 'clicks_R', 'THD_L', 'THD_R', 'wow', 'stereo_bleed'};
-
-csvdata = {};
-csvdata = {csvdata; headers};
+csvdata = {'date', 'pressing', 'top_stamper', 'top_hits', 'bottom_stamper', 'bottom_hits', 'record', 'side', 'track', 'lagdiff', 'normalization', 'RMS_L', 'RMS_R', 'clicks_L', 'clicks_R', 'THD_L', 'THD_R', 'wow', 'stereo_bleed'};
+% size(headers)
+% csvdata = {};
+% csvdata = {headers};
+size(csvdata)
 for i = (1:length(files))
     file = strcat(files(i).folder,'/',files(i).name)
 
@@ -26,11 +27,21 @@ for i = (1:length(files))
     track  = 0;
     
     %STRIP RELEVANT INFO FROM NAME 
-    [lagdiff, normalization, RMS_L, RMS_R, clicks_L, clicks_R, THD_L, THD_R, wow, stereo_bleed] = RecordProcess(file);
+    filename = files(i).name
+    date = filename(1:6)
+    pressing = filename(8:16)
+    record = filename(19:20)
+    side = filename(21)    
+    top_stamper = filename(8)
+    top_hits = filename(9:12)
+    bottom_stamper = filename(13)
+    bottom_hits = filename(14:17)
 
-    
-    % csvdata = {csvdata; date, pressing, top_stamper, top_hits, bottom_stamper, bottom_hits, record, side, track, lagdiff, normalization, RMS_L, RMS_R, clicks_L, clicks_R, THD_L, THD_R, wow, stereo_bleed}
-    csvdata(end+1) = {date, pressing, top_stamper, top_hits, bottom_stamper, bottom_hits, record, side, track, lagdiff, normalization, RMS_L, RMS_R, clicks_L, clicks_R, THD_L, THD_R, wow, stereo_bleed}
+
+
+    [lagdiff, normalization, RMS_L, RMS_R, clicks_L, clicks_R, THD_L, THD_R, wow, stereo_bleed] = RecordProcess(file);
+    csvdata
+    csvdata(end+1,:) = {date, pressing, top_stamper, top_hits, bottom_stamper, bottom_hits, record, side, track, lagdiff, normalization, RMS_L, RMS_R, clicks_L, clicks_R, THD_L, THD_R, wow, stereo_bleed}
 
     
 end
