0492607d2ad05df9d9c31dd5a716a502f54a0f6a
cz <christopherzaworski@gmail.com>
2019-10-21 01:00:14 -0400

    press process now finds nearest press close position

--- matlab_code/PressProcess.m
+++ matlab_code/PressProcess.m
@@ -2,9 +2,8 @@
 addpath('E:\audio_files\A0000B0000\')
 folder = ('D:\OneDrive - University of Waterloo\Vinyl_Project\data\121918_A0000B0000\')
 
-addpath('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/') 
-folder = ('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/data/
-121918_A0000B0000/')
+% addpath('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/audio_bin/') 
+% folder = ('/Users/cz/OneDrive - University of Waterloo/Vinyl_Project/data/121918_A0000B0000/')
 
 
 
@@ -40,6 +39,7 @@ opts = setvartype(opts,selectedColumns,'string');
 opts.SelectedVariableNames = selectedColumns;
 
 SensorValues = readtable(strcat(folder,date_tag,'_SensorValues.csv'),opts);
+SensorValues = [SensorValues; readtable(strcat(folder,'2012-12-20_SensorValues.csv'),opts)];
 
 %#####_JobDetailsCurrent
 % tracks cycle times
@@ -47,11 +47,13 @@ SensorValues = readtable(strcat(folder,date_tag,'_SensorValues.csv'),opts);
 
 
 JobDetails = readtable(strcat(folder,date_tag,'_JobDetailsCurrent.csv'));
+JobDetails = [JobDetails; readtable(strcat(folder,'2012-12-20_JobDetailsCurrent.csv'))];
 
 
 %#####_ADAPT_DATA
 % tracks the press options changes via timestamps
 ADAPT = readtable(strcat(folder,'121918_CZA0000B0000_ADAPT_DATA.xlsx'));
+ADAPT = [ADAPT; readtable(strcat(folder,'122018_CZA0000B0000_ADAPT_DATA.xlsx'))];
 
 %#####_TimeStamps
 % Recorded by hand timestamps
@@ -61,26 +63,67 @@ SensorValues.RecordTimeStamp = datetime(SensorValues.RecordTimeStamp, 'InputForm
 TimeStamps.TimeStamp = datetime(TimeStamps.TimeStamp, 'InputFormat', 'MMMM d, yyyy hh:mm:ss');
 
 for i = (1:length(TimeStamps.TimeStamp))
+    % if TimeStamps.TimeStamp(i) > datetime(date_tag)
+    %     date_tag
+    %     continue
+    % end
+    % disp('FOUND MATCH')
     [closestTimeStamp,closestIndex] = min(abs(SensorValues.RecordTimeStamp-TimeStamps.TimeStamp(i)));
     if isnan(closestTimeStamp)
         continue
     end
+    disp(strcat(string(SensorValues.RecordTimeStamp(closestIndex)),'.....',string(TimeStamps.TimeStamp(i))))
 
-    % TimeStamps.TimeStamp(i)
+    
     % SensorValues.RecordTimeStamp(closestIndex)
     % SensorValues.PressPosition_Inches(closestIndex)
-    SensorValues.PressForce_Ton(closestIndex)
+    % SensorValues.PressForce_Ton(closestIndex)
 
     % find the max or min PressForce in a nearby area, 
     %write needed values to table  
-    if SensorValues.PressForce_Ton(closestIndex) > 500; 
-        disp('Press closed')
-
+    if str2num(SensorValues.PressPosition_Inches(closestIndex)) > 1 
+        % disp('Press closed')
+        SensorValues.PressPosition_Inches(closestIndex);
     else
-        disp('Press open')
-    end
+        % disp('Press open') 
+        SensorValues.PressPosition_Inches(closestIndex);
+        for k = (1:10)
+            pl = str2double(SensorValues.PressPosition_Inches(closestIndex - k));
+            pr = str2double(SensorValues.PressPosition_Inches(closestIndex + k));
+            
+            if pl > 1
+                closestIndex = closestIndex - k;
+                % SensorValues.PressPosition_Inches(closestIndex) 
+                break
+            end
+            if pr > 1
+                closestIndex = closestIndex + k;
+                % SensorValues.PressPosition_Inches(closestIndex)
+                break
+            end
+        end
+    end 
 
-    closestIndex
+    j = closestIndex
+    while SensorValues.PressPosition_Inches(j) > 1
+        j = j - 1
+    end
+    press_open = j; 
+    while SensorValues.PressPosition_Inches(1) < 1
+        j = j - 1
+    end
+    upper_bound = j;
+
+
+    j = closestIndex
+    while SensorValues.PressPosition_Inches(j) > 1
+        j = j + 1
+    end
+    press_open = j; 
+    while SensorValues.PressPosition_Inches(1) < 1
+        j = j + 1
+    end
+    upper_bound = j;
 
 end
 
