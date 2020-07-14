3e27b921d90124eb36ca076ed578810c3f7010fe
Chris Zaworski <christopherzaworski@gmail.com>
2020-07-14 14:37:10 -0400

    got fft and normalization working. Seperate Tracks crashes, gotta figure that out

--- matlab_code/1kHzTestTone/normalizationtest.m
+++ matlab_code/1kHzTestTone/normalizationtest.m
@@ -15,47 +15,35 @@ seg = seg.';
 figure(1)
 plot(seg)
 
-size(seg)
-
 figure(2)
-% win = flattopwin(L);
-% seg2 = seg.*win;
-seg2 = seg;
-N = 2^12;
-xdft = fft(seg2, N)/N;
-xdft = xdft(1:N/2+1);
-psdx = (1/(9600*N)) * abs(xdft).^2;
-xdft(2:end-1) = 2*xdft(2:end-1);
-fdft = fs*(0:(N/2))/N;
-
-size(xdft)
-size(fdft)
-
-plot(fdft,20*log10(abs(xdft)))
-% plot(fdft,10*log10(abs(psdx)))
-grid on 
-set(gca, 'XScale', 'log');
-xlabel('Frequency (Hz)')
-ylabel('Level (dB)')  
-
-disp(strcat('max xdft... ', num2str(20*log10(max(abs(xdft))))))
-
-
-% max(seg)
-% max(abs(xdft))
-% 20*log10(max(abs(xdft)))
-
-[data_fft, freq_fft] = audio_spectrum(seg2, fs, 1, 2^12);
+[data_fft, freq_fft] = audio_spectrum(seg, fs, 1, 2^12);
 % disp(strcat('max(abs(data_fft))... ', num2str(20*log10(max(abs(data_fft))))))
-figure(3)
 audio_plotspectrum(freq_fft, data_fft, 'audio_spectrum');
 
-disp(strcat('max(abs(data_fft))', num2str(max(abs(data_fft)))))
-disp(strcat('max(abs(xdft))', num2str(max(abs(xdft)))))
+disp(strcat('before norm... ', num2str(max(abs(data_fft)))))
 
 segRMS = rms(seg);
 disp(strcat('segRMS root 2... ', num2str(segRMS*sqrt(2))))
 
+normalization = segRMS*sqrt(2);
+seg2 = seg/normalization;
+
+figure(3)
+plot(seg2)
+
+figure(4)
+[data_fft, freq_fft] = audio_spectrum(seg2, fs, 1, 2^12);
+audio_plotspectrum(freq_fft, data_fft, 'audio_spectrum');
+disp(strcat('after norm... ', num2str(max(abs(data_fft)))))
+
+win = flattopwin(L);
+figure(10)
+plot(win)
+seg3 = seg2.*win;
 
 
+figure(5)
+[data_fft, freq_fft] = audio_spectrum(seg3, fs, 1, 2^12);
+audio_plotspectrum(freq_fft, data_fft, 'audio_spectrum');
+disp(strcat('after window... ', num2str(max(abs(data_fft)))))
 
